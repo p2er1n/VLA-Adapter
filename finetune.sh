ad942622edc42274b2462c1b5b1b549e41c98c39
Peerin <s1461200635@gmail.com>
2026-03-02 18:19:56 +0800

    feat: 添加VLM微调脚本支持真实世界数据集
    添加了finetune.sh脚本用于配置和执行VLM模型在LIBERO数据集上的微调任务，
    包括模型路径、数据目录、训练参数等完整配置，并支持LoRA微调、日志记录和
    wandb实验跟踪功能。

---
+++ finetune.sh
@@ -0,0 +1,38 @@
+vlm_path=/root/autodl-tmp/pretrained_models/prism-qwen25-extra-dinosiglip-224px-0_5b
+data_root_dir=/root/autodl-tmp/data/libero
+data_name=pick_and_place_1_50
+run_root_dir=/root/autodl-tmp/outputs
+logs_dir=/root/autodl-tmp/logs
+
+bs=8
+grad_acc_steps=2
+lr=2e-4
+
+current_time=$(date +"%Y-%m-%d-%H-%M-%S")
+CUDA_VISIBLE_DEVICES=0 torchrun --standalone --nnodes 1 --nproc-per-node 1 vla-scripts/finetune.py \
+--vlm_path "$vlm_path" \
+--config_file_path pretrained_models/configs \
+--data_root_dir "$data_root_dir" \
+--dataset_name $data_name \
+--run_root_dir "$run_root_dir" \
+--use_film False \
+--num_images_in_input 2 \
+--use_proprio True \
+--use_lora True \
+--use_fz False \
+--use_minivlm True \
+--image_aug True \
+--num_steps_before_decay 200000 \
+--max_steps 200005 \
+--save_freq 5000 \
+--save_latest_checkpoint_only False \
+--merge_lora_during_training True \
+--batch_size "$bs" \
+--grad_accumulation_steps "$grad_acc_steps" \
+--learning_rate "$lr" \
+--lora_rank 64 \
+--use_pro_version True \
+--wandb_entity "sjh-xidian-university" \
+--wandb_project "VLA-Adapter-realworld" \
+--run_id_note VLA-Adapter-realworld--"$data_name"--"$bs-$grad_acc_steps-$lr"--$current_time \
+> "$logs_dir"/VLA-Adapter-realworld--"$data_name"--"$bs-$grad_acc_steps-$lr"--$current_time.log 2>&1 &
