991c86592e260bc3dff3658f89e7841f24ea1b30
Peerin <s1461200635@gmail.com>
2026-03-02 18:15:01 +0800

    feat(rlds/oxe): 添加RealWorld数据集转换支持
    - 新增pick_and_place_1_50数据集到OXE标准转换配置中
    - 使用libero_dataset_transform处理RealWorld数据集

--- prismatic/vla/datasets/rlds/oxe/transforms.py
+++ prismatic/vla/datasets/rlds/oxe/transforms.py
@@ -960,4 +960,6 @@ OXE_STANDARDIZATION_TRANSFORMS = {
     "aloha1_fold_shirt_30_demos": aloha_dataset_transform,
     "aloha1_scoop_X_into_bowl_45_demos": aloha_dataset_transform,
     "aloha1_put_X_into_pot_300_demos": aloha_dataset_transform,
+    ### RealWorld datasets
+    "pick_and_place_1_50": libero_dataset_transform,
 }
